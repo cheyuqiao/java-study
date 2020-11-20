7ffd87b3356c600fc3cddf86a15e09ab7c35e7a0
cheyuqiao <295497738@qq.com>
2020-11-20 14:26:10 +0800

    feat #java8 stream和contains的测试

---
+++ src/source/ContainsTest.java
@@ -0,0 +1,48 @@
+package source;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ContainsTest {
+    public static void main(String[] args) {
+        List<String> nameList = new ArrayList<>();
+        nameList.add("tom");
+        nameList.add("jack");
+
+        List<Person> peopleList = new ArrayList<>();
+        peopleList.add(new Person("tom", 15));
+        peopleList.add(new Person("jack", 16));
+        peopleList.add(new Person("peter", 16));
+        
+        // 筛选，获取其中的字段
+        List<String> users = peopleList.stream().filter(person -> person.getAge() == 16).map(Person::getName).collect(Collectors.toList());
+        // 筛选一个list中的某个字段，不在另外一个list中
+        peopleList = peopleList.stream().filter(person -> !nameList.contains(person.getName())).collect(Collectors.toList());
+    }
+}
+class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
